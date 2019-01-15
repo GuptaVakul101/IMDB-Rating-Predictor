b4c0a6687768fec9aa0fc01910d29ecfeb6934df
VakulGupta <vakulgupta101@gmail.com>
2019-01-15 19:31:11 +0530

    Added code to scrape comments

--- scrape.py
+++ scrape.py
@@ -1,6 +1,9 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
 
 def GET_SOUP(my_url):
 	url = my_url
@@ -16,7 +19,9 @@ table = soup.find('tbody', attrs={'class': 'lister-list'})
 
 listofRows=[]
 count = 0
+
 for row in table.findAll('tr'):
+
     listOfCells = []
     rating = row.find('td', attrs={'class': 'ratingColumn imdbRating'}).text.replace('\n', '')
     title = row.find('td', attrs={'class': 'titleColumn'}).find('a').text.replace('\n', '')
@@ -25,11 +30,32 @@ for row in table.findAll('tr'):
     
     url_2="https://www.imdb.com"+row.find('td', attrs={'class': 'titleColumn'}).find('a').get("href")
     soup_2=GET_SOUP(url_2)
-    #print(url_2)
+    # print(url_2)
     url_3="https://www.imdb.com" + soup_2.find('div', attrs={'class': 'user-comments'}).findAll('a')[4].get("href")
-    soup_3=GET_SOUP(url_3)
-    #print(url_3)
     
+    # print(url_3)
+
+    count_2=0
+    driver = webdriver.Chrome('/usr/bin/chromedriver')
+    driver.get(url_3)
+    while True:
+    	try:
+    		count_2+=1
+    		loadMoreButton = driver.find_element_by_id("load-more-trigger")
+    		time.sleep(1)
+    		loadMoreButton.click()
+    		time.sleep(1)
+    		if count_2==10:
+    			break
+    	except Exception as e:
+    		print(e)
+    		break
+    print("Complete")
+    #time.sleep(10)
+
+    soup_3=BeautifulSoup(driver.page_source)    
+
+
     print("Start:", str(count))
     for item in soup_3.findAll('div', attrs={'class': 'review-container'}):
     	text_2=item.find('div', attrs={'class': 'lister-item-content'}).find('a').text
@@ -41,6 +67,8 @@ for row in table.findAll('tr'):
     count += 1
     if count == 2:
     	break
+
+    driver.quit()
 
 outfile = open("./imdb.csv", "w")
 writer = csv.writer(outfile)
